86cca77a2bc22ee6b8bdb933865a2c88adc3893a
halbalbalhal <aleksandrbirula3@gmail.com>
2023-01-28 12:39:50 +0300

    mobile menu update

--- scripts/burgerscript.js
+++ scripts/burgerscript.js
@@ -1,19 +1,23 @@
 const burgerButton = document.querySelector('.nav__burger')
 const burgerMenu = document.querySelector('.burger__menu')
 const closeButton = document.querySelector('.close__button')
+const closeLogo = document.querySelector('.nav__logo')
 
 function menuActivate() {
     burgerMenu.style.display = 'flex'
     burgerButton.style.display = 'none'
+    closeLogo.style.display = 'none'
 }
 
 function menuClose() {
     burgerMenu.style.display = 'none'
+    closeLogo.style.display = 'flex'
+    burgerButton.style.display = 'flex'
 	window.addEventListener("resize", function() {
-        if (window.matchMedia("(max-width: 1000px)").matches) {
-            burgerButton.style.display = 'flex'
-        } else {
+        if (window.matchMedia("(min-width: 1000px)").matches) {
             burgerButton.style.display = 'none'
+        } else {
+            burgerButton.style.display = 'flex'
         }
     })
 }
